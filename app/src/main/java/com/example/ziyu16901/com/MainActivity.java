a277dbc30d672ff33bccdff27ecbcd01de59a766
beansYan <913244892@qq.com>
2016-01-06 09:42:08 +0800

    增加了备忘录，分享到微信

--- app/src/main/java/com/example/ziyu16901/com/MainActivity.java
+++ app/src/main/java/com/example/ziyu16901/com/MainActivity.java
@@ -2,30 +2,37 @@ package com.example.ziyu16901.com;
 
 
 
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.support.design.widget.FloatingActionButton;
-        import android.support.design.widget.NavigationView;
-        import android.support.design.widget.Snackbar;
-        import android.support.design.widget.TabLayout;
-        import android.support.v4.app.Fragment;
-        import android.support.v4.app.FragmentManager;
-        import android.support.v4.app.FragmentPagerAdapter;
-        import android.support.v4.view.GravityCompat;
-        import android.support.v4.view.ViewPager;
-        import android.support.v4.widget.DrawerLayout;
-        import android.support.v7.app.ActionBar;
-        import android.support.v7.app.AppCompatActivity;
-        import android.support.v7.widget.Toolbar;
-        import android.view.Menu;
-        import android.view.MenuItem;
-        import android.view.View;
-        import android.widget.AdapterView;
-        import android.widget.ArrayAdapter;
-        import android.widget.Toast;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-        import java.util.ArrayList;
-        import java.util.List;
+import com.example.ziyu16901.com.Fragerment.fenlei;
+import com.example.ziyu16901.com.Fragerment.huodong;
+import com.example.ziyu16901.com.Note.MainShow;
+import com.example.ziyu16901.com.Note.NoteActivity;
+import com.example.ziyu16901.com.Share.ShareToWeChat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * TODO
@@ -33,6 +40,8 @@ package com.example.ziyu16901.com;
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
+    private com.example.ziyu16901.com.Note.ActivityManager am;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,8 +84,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                intent = new Intent(MainActivity.this,NoteActivity.class);
+                startActivity(intent);
+
             }
         });
 
@@ -86,15 +96,46 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.sample_actions, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
         switch (item.getItemId()) {
             case android.R.id.home:
                 mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.about:
+                AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
+                adb.setTitle("关于我们");
+                adb.setMessage("江西师大软件学院\n\t X3505工作室－－颜扬君，曾玲超");
+                adb.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "谢谢",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+                adb.show();
+                break;
+            case R.id.note:
+                intent = new Intent(MainActivity.this,ShareToWeChat.class);
+                startActivity(intent);
+                return true;
+            case R.id.exit:
+                AlertDialog.Builder adb2 = new AlertDialog.Builder(MainActivity.this);
+                adb2.setTitle("消息");
+                adb2.setMessage("真的要退出吗？");
+                adb2.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        am.exitAllProgress();
+                    }
+                });
+                adb2.setNegativeButton("取消", null);
+                adb2.show();
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -117,9 +158,28 @@ public class MainActivity extends AppCompatActivity {
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        menuItem.setChecked(true);
-                        startActivity(new Intent(MainActivity.this, LoginActivity.class));
-//                        mDrawerLayout.closeDrawers();
+//
+//                        switch (menuItem.getItemId()) {
+//                            case R.idzzz.nav_Login:
+//                                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+//                                break;
+//                            case R.id.nav_Persona_Center:
+//                                startActivity(new Intent(MainActivity.this, PersonalCenter.class));
+//                                break;
+//                            case R.id.nav_My_Organization:
+//                                startActivity(new Intent(MainActivity.this, MyOrganization.class));
+//                                break;
+//                            case R.id.nav_My_Activity:
+//                                startActivity(new Intent(MainActivity.this, MyActivity.class));
+//                                break;
+//                            case R.id.nav_Setting:
+//                                startActivity(new Intent(MainActivity.this, SettingsActivity.class));
+//                                break;
+//                        }
+
+                         menuItem.setChecked(true);
+                        startActivity(new Intent(MainActivity.this, Login.class));
+                        mDrawerLayout.closeDrawers();
                         return true;
                     }
                 });
@@ -153,7 +213,11 @@ public class MainActivity extends AppCompatActivity {
             return mFragmentTitles.get(position);
         }
     }
+    public class ActivityManager {
+
+        private List<Activity> list;
 
 
+    }
 }
 
